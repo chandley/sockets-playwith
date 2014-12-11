eeae6634dd289eb3768f7f232554c69726200b1a
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-11 15:42:12 +0000

    can send message from client to server by pressing button

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -1,5 +1,15 @@
 var socket = io.connect('/');
 
-$('button').click(function(){
-  socket.emit('Hi from client, {message: "hello there Simon", niceThing: "hair"}');
+$(document).ready(function(){
+  console.log('ready ready');
+
+  $("#like").click(function(){
+    console.log("you clicked button");
+    socket.emit("Hi from client", {message: 'hello there Simon', niceThing: 'hair'});
+  });
+
+
+
+
+
 });
