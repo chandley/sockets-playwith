1f7cb7742347fe8f72790ea7e088a569019f094a
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-11 12:24:48 +0000

    user disconnect message server side

--- src/socketController.js
+++ src/socketController.js
@@ -1,7 +1,13 @@
 var socket = function(io){
 
-  io.on('connection', function(){
+  io.on('connection', function(socket){
     console.log ('Client Connected');
+
+    socket.on('disconnect', function(){
+      // io.sockets.emit('user disconnected');
+      console.log ('Server says bye');
+     });
+
   });
 
 };
