0659c19ed5b85da5d205eeb3abbc4218a4832542
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-11 14:11:15 +0000

    messaging to server looks good

--- src/socketController.js
+++ src/socketController.js
@@ -3,10 +3,18 @@ var socket = function(io){
   io.on('connection', function(socket){
     console.log ('Client Connected');
 
+    socket.on('private message', function (msg) {
+      console.log('I received a private message saying ', msg.msg);
+    });
+
     socket.on('disconnect', function(){
       // io.sockets.emit('user disconnected');
       console.log ('Server says bye');
-     });
+    });
+
+    socket.on('Hi from client', function(message){
+      console.log(message.message + ' your ' + message.niceThing + ' looks nice');
+    })
 
   });
 
