62e799e28dfbfeff1b365a154c553077c0a1001a
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-11 12:02:30 +0000

    socket connectin is set up no errors

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 var ejs = require('ejs');
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response){
   // response.send("Hello world");
   response.render('index');
 });
+
+app.get('/sockets', function(request, response){
+  response.render('sockets');
+});
+
 
 server.listen(3000, function(){
   console.log("Server listening on port 3000");
